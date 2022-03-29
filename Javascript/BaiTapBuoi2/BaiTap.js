c378d1af80d7e0d74743eade95af44ced2013be7
hoangdat3179 <hoangdat3179@gmail.com>
2022-03-29 17:15:48 +0700

    Update BaiTap.js

--- Javascript/BaiTapBuoi2/BaiTap.js
+++ Javascript/BaiTapBuoi2/BaiTap.js
@@ -24,6 +24,51 @@ let products = [
     count: 3,
   },
 ];
+// 1. In ra thông tin các sản phẩm trong giỏ hàng theo cấu trúc sau
+// Tên - Giá - Thương Hiệu - Số lượng
+// Ví dụ : OPPO Find X3 Pro - 19500000 - OPPO - 3
+
+function displayInfo(arr){
+  arr.forEach(products => {
+      console.log(`${products.name} - ${products.price} - ${products.brand} - ${products.count}`)
+  });
+}
+displayInfo(products);
+
+
+// 2. Tính tổng tiền tất cả sản phẩm trong giỏ hàng
+// Tổng tiền mỗi sản phẩm = price * count
+
+function calculatorTotalMoney(arr){
+  let sum = 0;
+  arr.forEach(products =>{
+      sum += products.price *products.count
+  })
+  return sum;
+}
+console.log(calculatorTotalMoney(products));
+
+// 3. Tìm các sản phẩm của thuơng hiệu "Apple"
+
+function findByBrand(arr,brandName){
+  return arr.filter(products => products.brand.toLowerCase() == brandName.toLowerCase())
+}
+console.log(findByBrand(products, "Apple"));
+
+// 4. Tìm các sản phẩm có giá > 20000000
+
+function findByPrice(arr,price){
+  return arr.filter(products => products.price > price)
+}
+
+console.log(findByPrice(products,20000000))
+
+// 5. Tìm các sản phẩm có chữ "pro" trong tên (Không phân biệt hoa thường)
+
+function findByName(arr,name){
+  return arr.filter(products => products.name.toLowerCase().includes(name.toLowerCase()))
+}
+console.log(findByName(products, "PRO"));
 
 // 6. Thêm 1 sản phẩm bất kỳ vào trong mảng product
 function addProduct() {
